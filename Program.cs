25ebec5e14e671c9fc61b18405ab551adee9a591
RhojemJR <165289610+RhojemJR@users.noreply.github.com>
2024-06-14 19:27:15 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace prac
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            int birthyear = yearofbirth();
+			string profile = Getage(birthyear);
+			Console.WriteLine(profile);
+        }
+		
+		static string Getage(int birthyear)
+		{
+            int age = 2024 - birthyear;
+            string generation = "";
+            string stage = "";
+
+            if (age >= 79 && age <= 96)
+            {
+                generation = "The Silent Generation";
+            }
+            else if (age >= 60 && age <= 78)
+            {
+                generation = "Baby Boomers";
+            }
+            else if (age >= 44 && age <= 59)
+            {
+                generation = "Gen X";
+            }
+            else if (age >= 28 && age <= 43)
+            {
+                generation = "Millenials";
+            }
+            else if (age >= 12 && age <= 27)
+            {
+                generation = "Gen Z";
+            }
+            else if (age >= 0 && age <= 11)
+            {
+                generation = "Gen Alpha";
+            }
+
+            if (age >= 0 && age <= 3)
+            {
+                stage = "Infant";
+            }
+            else if (age >= 4 && age <= 6)
+            {
+                stage = "Early Childhood";
+            }
+            else if (age == 7 || age == 8)
+            {
+                stage = "Middle Childhood";
+            }
+            else if (age >= 9 && age <= 11)
+            {
+                stage = "Late Childhood";
+            }
+            else if (age >= 12 && age <= 20)
+            {
+                stage = "Adolescence";
+            }
+            else if (age >= 21 && age <= 35)
+            {
+                stage = "Early Adulthood";
+            }
+            else if (age >= 36 && age <= 50)
+            {
+                stage = "Midlife";
+            }
+            else if (age >= 51 && age <= 80)
+            {
+                stage = "Mature Adulthood";
+            }
+            else if (age > 80)
+            {
+                stage = "Late Adulthood";
+            }
+
+            return$"{age} years old, {generation}, {stage}";
+        }
+		
+		static int yearofbirth()
+		{
+            Console.Write("Enter your Birthyear: ");
+            int birthyear = Convert.ToInt16(Console.ReadLine());
+			return birthyear;
+
+		}
+	}
+}
+
